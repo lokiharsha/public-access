811e69904499bfbda9dad2f04359ff3088622e9c
lokiharsha <lakshmipathi.devops76@gmail.com>
2024-08-20 19:25:04 +0530

    changes happen

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
   public static void main(String args[]){
-    System.out.println("welcome");
+    	String str="welcome";
+	System.out.println(str);
+	System.out.println(str.toUpperCase());
   }
 }
