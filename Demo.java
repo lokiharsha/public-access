14abbd61978f59d03ef4e6a6eeaaa0dd2fbd57ce
siripireddy1998 <130535382+siripireddy1998@users.noreply.github.com>
2024-08-20 19:44:45 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
   public static void main(String args[]){
-    	String str="lakshmi";
+    	String str="chandra";
 	System.out.println(str);
 	System.out.println(str.toUpperCase());
   }
