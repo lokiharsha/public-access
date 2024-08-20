e377f9fc43c147fa293822dce9010c7137277185
lokiharsha <44457615+lokiharsha@users.noreply.github.com>
2024-08-20 19:43:12 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
   public static void main(String args[]){
-    	String str="welcome";
+    	String str="lakshmi";
 	System.out.println(str);
 	System.out.println(str.toUpperCase());
   }
